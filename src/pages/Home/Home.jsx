30615043b85021b91b2aae19e698281f7ac9efe2
ainoacp <118761900+ainoacp@users.noreply.github.com>
2023-02-16 19:35:28 +0000

    styles_home_1

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,15 +3,15 @@ import { NavLink } from "react-router-dom";
 export default function Home() {
 
     return (
-        <>
-            <div>
+        <div className="c-home-container">
+            <div className="c-home-title">
                 <h3>¿Cómo quieres entrar?</h3>
             </div>
-            <div>
+            <div className="c-home-buttons">
                 <NavLink to="/register">Usuario</NavLink>
                 <NavLink to="/register">Asociación</NavLink>
                 <button>Registrarse en otro momento</button>
             </div>
-        </>
+        </div>
     )
 }

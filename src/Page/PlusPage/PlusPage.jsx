ec23a3f9d8d52ea9dca1f42326c4e67e52094fac
Jonay82 <jrggrj0design@gmail.com>
2023-02-17 10:55:47 +0000

    navbar/profile/plus

--- src/Page/PlusPage/PlusPage.jsx
+++ src/Page/PlusPage/PlusPage.jsx
@@ -6,9 +6,22 @@ import Navbar from '../../components/Navbar/Navbar';
 const PlusPage = () => {
 
   return (
-    <div>
-      <Navbar></Navbar>
-    </div>
+    <>   
+    <div className='container-profile'>
+          
+          <div className='container-start'>
+              <div className='box'><img className='picture-main'  alt=""></img>Asociaciones protectoras</div>
+              <div className='box'><img className='picture-main' alt=""></img>Eventos</div>
+              <div className='box'><img className='picture-main' alt=""></img>Curiosidades</div>
+              <div className='box'><img className='picture-main' alt=""></img>Ayuda</div>
+              <div className='box'><img className='picture-main' alt=""></img>Configuración</div>
+          </div>
+          <div className='container-end'>           
+              <div className='box'><img className='picture-main' alt=""></img>Cerrar sesión</div>
+          </div>
+      </div>
+      <Navbar></Navbar>   
+    </>
   )
 }
 
--- src/Page/ProfilePage/ProfilePage.jsx
+++ src/Page/ProfilePage/ProfilePage.jsx
@@ -8,15 +8,18 @@ const ProfilePage = () => {
   return (
     <>
       <div className='container-profile'>
-          <div className='profile-picture'></div>
-          <div className='box'></div>
-          <div className='box'></div>
-          <div className='box'></div>
-          <div className='box'></div>
-          <br></br>
-          <div className='box'></div>
-          <div className='box'></div>
-          <div className='box'></div>
+          <div className='profile-picture'><img className='picture-profile'  alt=""></img></div>
+          <div className='container-start'>
+              <div className='box'><img className='picture-main'  alt=""></img>Mi perfil</div>
+              <div className='box'><img className='picture-main' alt=""></img>Direcciones</div>
+              <div className='box'><img className='picture-main' alt=""></img>Favoritos</div>
+              <div className='box'><img className='picture-main' alt=""></img>Notificaciones</div>
+          </div>
+          <div className='container-end'>
+              <div className='box'><img className='picture-main' alt=""></img>Estado de la adopción</div>
+              <div className='box'><img className='picture-main' alt=""></img>Apadrinar</div>
+              <div className='box'><img className='picture-main' alt=""></img>Donar</div>
+          </div>
       </div>
       <Navbar></Navbar>
     </>
